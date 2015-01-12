123802182a1ce1071e6d35fdf6f19bc7c492aba3
Ryan <ryanpauley@gmail.com>
2015-01-13 10:20:37 +1100

    HOPEFULLY

--- index.js
+++ index.js
@@ -21,79 +21,64 @@ app.set('transports', [
     	'jsonp-polling'
     ]);
 
-console.log("\n\n\n\n\n\n\nDID THIS SHIT HELP?\n\n\n\n\n\n\n\n")
-
-// res.header("origins","*:*");
-// res.header('Access-Control-Allow-Origin', '*:*');
-// res.header('Access-Control-Allow-Credentials', true);
-// res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-// res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
 var allowCrossDomain = function(req, res, next) {
-res.header('Access-Control-Allow-Origin', 'http://yaps.herokuapp.com');
-res.header('Access-Control-Allow-Credentials', true);
-res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-// debugger;
-// intercept OPTIONS method
-if ('OPTIONS' == req.method) {
-  res.send(200);
-}
-else {
-  next();
-}
+    res.header('Access-Control-Allow-Origin', 'http://yaps.herokuapp.com');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
 };
 
 app.use(allowCrossDomain);
 
 
-console.log("\n\n\n\n\n\n\n\n\n\n\n\n", app, "\n\n\n\n\n\n\n\n\n\n\n\n\n");
 
-console.log("\n\n\n\n\n\nWHAT THE FUCK\n\n\n\n\n\n")
+var url = "/";
 
+app.get(url, function(req, res){
+  res.sendfile('index.html');
+});
 
+io.on('connection', function(socket){
+	console.log('a user connected');
+	socket.on('disconnect', function(){
+		console.log('user disconnected');
+	});
+});
 
+io.on('connection', function(socket){
+	socket.on('chat message', function(msg){
+		console.log('message: ' + msg);
+	});
+});
 
-// var url = "/";
+// // // broadcast to all
+// // io.emit('some event', { for: 'everyone' });
 
-// app.get(url, function(req, res){
-//   res.sendfile('index.html');
-// });
-
-// io.on('connection', function(socket){
-// 	console.log('a user connected');
-// 	socket.on('disconnect', function(){
-// 		console.log('user disconnected');
-// 	});
-// });
-
-// io.on('connection', function(socket){
-// 	socket.on('chat message', function(msg){
-// 		console.log('message: ' + msg);
-// 	});
-// });
-
-// // // // broadcast to all
-// // // io.emit('some event', { for: 'everyone' });
-
-// // // // broadcast to everyone except for certain socket
-// // // io.on('connection', function(socket){
-// // // 	socket.broadcast.emit('hi');
-// // // });
-
-// io.on('connection', function(socket){
-// 	socket.on('chat message', function(msg){
-// 		io.emit('chat message', msg);
-// 	});
-// });
-
-// // // http.listen(3001, function(){
-// // //   console.log('listening on *:3001');
-// // // });
-
-
-// // http.listen(url, function(){
-// //   console.log('listening to' + url);
+// // // broadcast to everyone except for certain socket
+// // io.on('connection', function(socket){
+// // 	socket.broadcast.emit('hi');
 // // });
 
-// // // io.connect('http://yaps.herokuapp.com');
+io.on('connection', function(socket){
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+	});
+});
+
+// // http.listen(3001, function(){
+// //   console.log('listening on *:3001');
+// // });
+
+
+// http.listen(url, function(){
+//   console.log('listening to' + url);
+// });
+
+// // io.connect('http://yaps.herokuapp.com');
