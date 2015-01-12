b7a7cc0ac1302607c3b3ee1b2614ad189affb0a9
Ryan <ryanpauley@gmail.com>
2015-01-12 17:08:37 +1100

    adding functions to index.js for cross domain traffic

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.configure(function () {
+  app.use(allowCrossDomain);
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(application_root, "public")));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
