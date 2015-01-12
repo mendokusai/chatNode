de50b21ee14062709892c358544a0bc07ec75673
Ryan <ryanpauley@gmail.com>
2015-01-12 19:28:38 +1100

    Its cors

--- index.js
+++ index.js
@@ -14,7 +14,9 @@
 
 var app = require('express')();
 var http = require('http').Server(app);
+var cors = require('cors');
 var io = require('socket.io')(http);
+
 io.set('transports', [
         	'websocket',
         	'flashsocket',
@@ -47,9 +49,10 @@ var enableCORS = function(req, res, next) {
  
 // enable CORS!
 app.use(enableCORS);
+app.use(cors());
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://yaps.herokuapp.com');
+    res.header('Access-Control-Allow-Origin', '*:*');
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
