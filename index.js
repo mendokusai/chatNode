b81672666295a48889c96802e5db3203d5408c68
Ryan <ryanpauley@gmail.com>
2015-01-12 12:53:33 +1100

    all the node chat up and working

---
+++ index.js
@@ -0,0 +1,33 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('chat message', function(msg){
+  	console.log('message: ' + msg);
+  });
+});
+
+// // broadcast to all
+io.emit('some event', { for: 'everyone' });
+
+// // broadcast to everyone except for certain socket
+// io.on('connection', function(socket){
+// 	socket.broadcast.emit('hi');
+// });
+
+io.on('connection', function(socket){
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+	});
+});
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
