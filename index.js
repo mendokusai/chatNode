7be29fc5b82964ec6a5c5a3205d1118371d26ef7
Ryan <ryanpauley@gmail.com>
2015-01-12 18:58:32 +1100

    Removed Put, patch etc.

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var io = require('socket.io')(http);
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   return next();
 });
