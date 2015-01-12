5f97be7865c56ab36cb3bb056b6353f3959a7f94
Ryan <ryanpauley@gmail.com>
2015-01-12 19:21:27 +1100

    enableCors. Ugh

--- index.js
+++ index.js
@@ -28,6 +28,26 @@ io.configure( function(){
 });
 io.set("origins", "*");
 io.set("origins","*:*");
+
+// Enables CORS
+var enableCORS = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+ 
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+ 
+ 
+// enable CORS!
+app.use(enableCORS);
+
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
