ce2e7563b835d579c3d30ae3606a04c92dec2969
Ryan <ryanpauley@gmail.com>
2015-01-12 19:25:09 +1100

    ARGH

--- index.js
+++ index.js
@@ -49,7 +49,8 @@ var enableCORS = function(req, res, next) {
 app.use(enableCORS);
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://yaps.herokuapp.com');
+    res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
