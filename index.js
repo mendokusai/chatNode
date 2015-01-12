d8787e7a896bff4cde0fa0e6de0772d1d3fe4b76
Ryan <ryanpauley@gmail.com>
2015-01-13 09:10:56 +1100

    Changed to express 4

--- index.js
+++ index.js
@@ -12,18 +12,10 @@ io.set('transports', [
 
 io.set("origins","*:*");
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*:*');
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
+app.set('Access-Control-Allow-Origin', '*:*');
+app.set('Access-Control-Allow-Credentials', true);
+app.set('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+app.set('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
 app.use(allowCrossDomain);
 
