19cd6d3b96e92d09904fb36afba18671cadef1b9
Ryan <ryanpauley@gmail.com>
2015-01-13 09:34:51 +1100

    passing into allowCrossDomain with app.use

--- index.js
+++ index.js
@@ -10,11 +10,29 @@ app.set('transports', [
     	'jsonp-polling'
     ]);
 
-res.header("origins","*:*");
-res.header('Access-Control-Allow-Origin', '*:*');
-res.header('Access-Control-Allow-Credentials', true);
-res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+// res.header("origins","*:*");
+// res.header('Access-Control-Allow-Origin', '*:*');
+// res.header('Access-Control-Allow-Credentials', true);
+// res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+// res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*:*');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    // debugger;
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.use(allowCrossDomain);
+
 
 console.log("\n\n\n\n\n\n\n\n\n\n\n\n", app, "\n\n\n\n\n\n\n\n\n\n\n\n\n");
 
