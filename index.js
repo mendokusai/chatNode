caee6c8ccfab5afc888039f5e5d4f315eb57259c
Ryan <ryanpauley@gmail.com>
2015-01-13 10:33:05 +1100

    an

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.use( cors() );
+// app.use( cors() );
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
