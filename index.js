051a9c4f2c7fed4180c1b8ecc4c46886f0dadee7
Ryan <ryanpauley@gmail.com>
2015-01-13 10:10:30 +1100

    try something that wont work

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ console.log("\n\n\n\n\n\n\nDID THIS SHIT HELP?\n\n\n\n\n\n\n\n")
 // res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
 var allowCrossDomain = function(req, res, next) {
-res.header('Access-Control-Allow-Origin', '*:*');
+res.header('Access-Control-Allow-Origin', 'http://yaps.herokuapp.com');
 res.header('Access-Control-Allow-Credentials', true);
 res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
